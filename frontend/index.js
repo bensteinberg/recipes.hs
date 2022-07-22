08511daee1828d0a8442cbd0dc94460ed8f827e0
Ben Steinberg <ben@potswift.org>
2022-07-22 15:30:37 -0400

    Switch to Vue 3 and reindent

--- frontend/index.js
+++ frontend/index.js
@@ -1,56 +1,57 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 
-new Vue({
-    el: '#app',
-    data: {
-        recipes: [],
-        search: '',
-        loading: true,
-        errored: false
-    },
-    filters: {
-        toURL(s) {
-            return (s.startsWith('http') ? `<a href="${s}">${new URL(s).hostname}</a>` : s);
-        }
-    },
-    mounted: function () {
-        this.search = new URLSearchParams(window.location.search).get("search") || '';
-        fetch('recipes')
-            .then(response => response.json())
-            .then(json => this.recipes = json)
-            .catch(error => {
-                console.log(error)
-                this.errored = true
-            })
-            .finally(() => this.loading = false);
-    },
-    watch: {
-        search: function(newSearch, oldSearch) {
-            let stateObj = { search: "" };
-            history.pushState(stateObj, "search", "?search=" + newSearch);
-        }
-    },
-    computed: {
-        filteredRecipes: function () {
-            // latinise from https://stackoverflow.com/a/9667817/4074877
-            String.prototype.normalise = function() {
-                return this.latinise().toLowerCase();
-            }
-            let tokens = this.search.split(" ").filter(a => a);
-            return this.recipes.filter(function(r) {
-                let targets = r.ingredients.map(i => i.ingredient.normalise())
-                    .concat([r.name.normalise(), r.source.normalise()]);
-                return tokens.map(
-                    token => targets.map(
-                        target => target.includes(token.normalise()))
-                        .some(a => a))
-                    .every(a => a);
-            });
-        },
-        sortedRecipes: function() {
-            return this.filteredRecipes.sort(function(a, b) {
-                return a.name.localeCompare(b.name);
-            });
-        }
+createApp({
+  data() {
+    return {
+      recipes: [],
+      search: '',
+      loading: true,
+      errored: false
     }
-});
+  },
+  filters: {
+    toURL(s) {
+      return (s.startsWith('http') ? `<a href="${s}">${new URL(s).hostname}</a>` : s);
+    }
+  },
+  mounted: function () {
+    this.search = new URLSearchParams(window.location.search).get("search") || '';
+    fetch('recipes')
+      .then(response => response.json())
+      .then(json => this.recipes = json)
+      .catch(error => {
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false);
+  },
+  watch: {
+    search: function(newSearch, oldSearch) {
+      let stateObj = { search: "" };
+      history.pushState(stateObj, "search", "?search=" + newSearch);
+    }
+  },
+  computed: {
+    filteredRecipes: function () {
+      // latinise from https://stackoverflow.com/a/9667817/4074877
+      String.prototype.normalise = function() {
+        return this.latinise().toLowerCase();
+      }
+      let tokens = this.search.split(" ").filter(a => a);
+      return this.recipes.filter(function(r) {
+        let targets = r.ingredients.map(i => i.ingredient.normalise())
+            .concat([r.name.normalise(), r.source.normalise()]);
+        return tokens.map(
+          token => targets.map(
+            target => target.includes(token.normalise()))
+            .some(a => a))
+          .every(a => a);
+      });
+    },
+    sortedRecipes: function() {
+      return this.filteredRecipes.sort(function(a, b) {
+        return a.name.localeCompare(b.name);
+      });
+    }
+  }
+}).mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const path = require('path');
 const PATHS = {
-    app: path.resolve(__dirname,'frontend'),
-    vendor: path.resolve(__dirname,'vendor'),
-    build: path.resolve(__dirname,'build/frontend')
+  app: path.resolve(__dirname,'frontend'),
+  vendor: path.resolve(__dirname,'vendor'),
+  build: path.resolve(__dirname,'build/frontend')
 };
 module.exports = {
-    entry: {
-        app: [ PATHS.app + "/index.js",
-               PATHS.vendor + "/latinise.min.js"]
-    },
-    output: {
-        path: PATHS.build,
-        filename: 'bundle.js'
-    },
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.common.js'
-        }
+  entry: {
+    app: [ PATHS.app + "/index.js",
+           PATHS.vendor + "/latinise.min.js"]
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.esm-browser.prod.js'
     }
+  }
 };
